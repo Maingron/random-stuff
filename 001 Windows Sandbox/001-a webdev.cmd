815ec3285a5d7066137f040daa516a96a13701d9
Maingron <r@maingron.com>
2025-09-24 19:17:31 +0200

    Change installer download links and start arguments

--- 001 Windows Sandbox/001-a webdev.cmd
+++ 001 Windows Sandbox/001-a webdev.cmd
@@ -13,10 +13,9 @@ exit
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 0 /f && reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 0 /f && taskkill /f /im explorer.exe && start explorer.exe
 
 start "" "C:\wsl_scriptdir\installer\BraveBrowserStandaloneSilentNightlySetup.exe"
-start "" "C:\wsl_scriptdir\installer\ChromeStandaloneSetup64.exe"
-start "" "C:\wsl_scriptdir\installer\OperaSetup.exe"
-msiexec /i "C:\wsl_scriptdir\installer\Firefox Setup 124.0b9.msi"
-start "" "C:\Program Files\Mozilla Firefox\firefox.exe" "https://maingron.com"
+start "" "C:\wsl_scriptdir\installer\ChromeStandaloneSetup64.exe" "/silent /installer"
+start "" "C:\wsl_scriptdir\installer\OperaSetup.exe" "--silent=1 --desktop-shortcut=1 --import-browser-data=0 --launchopera=0 --launchbrowser=0 --enable-stats=0 --consent-given=0 --general-interests=0 --personalized-content=0 --personalized-ads=0 --setdefaultbrowser=0"
+start "" "C:\wsl_scriptdir\installer\Firefox Setup ESR.exe" "/S /TaskbarShortcut=true /DesktopShortcut=true /MaintenanceService=false" 
 @REM TODO: Add Opera GX again
 
 @REM powershell -Command Add-AppxPackage -Path "C:\wsl_scriptdir\installer\Microsoft.WindowsTerminal_1.19.10573.0_8wekyb3d8bbwe.msixbundle"
@@ -52,16 +51,18 @@ echo 	^</MappedFolders^>
 echo 	^<LogonCommand^>
 echo 		^<Command^>C:\wsl_scriptdir\001-a webdev.cmd^</Command^>
 echo 	^</LogonCommand^>
+echo 	^<Networking^>true^</Networking^>
+echo 	^<vGPU^>Enable^</vGPU^>
 echo ^</Configuration^>
 
 exit /b
 exit
 
 :downloadinstaller
-curl -o "installer\Firefox Setup 124.0b9.msi" -L "https://archive.mozilla.org/pub/firefox/releases/124.0b9/win64/en-US/Firefox%%20Setup%%20124.0b9.msi"
+curl -o "installer\Firefox Setup ESR.exe" -L "https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=win64&lang=en-US"
 curl -o "installer\ChromeStandaloneSetup64.exe" -L "https://dl.google.com/tag/s/appguid%%3D%%7B8A69D345-D564-463C-AFF1-A69D9E530F96%%7D%%26iid%%3D%%7B47D0D7C1-C2D1-5419-CF41-72C2A0088A55%%7D%%26lang%%3Den%%26browser%%3D3%%26usagestats%%3D0%%26appname%%3DGoogle%%2520Chrome%%26needsadmin%%3Dprefers%%26ap%%3Dx64-stable-statsdef_1%%26installdataindex%%3Dempty/chrome/install/ChromeStandaloneSetup64.exe"
 curl -o "installer\BraveBrowserStandaloneSilentNightlySetup.exe" -L "https://github.com/brave/brave-browser/releases/download/v1.82.170/BraveBrowserStandaloneSilentSetup.exe"
-curl -o "installer\OperaSetup.exe" -L "https://net.geo.opera.com/opera/stable/windows?utm_tryagain=yes&standalone=1&platform=win64"
+curl -o "installer\OperaSetup.exe" -L "https://download.opera.com/download/get/?id=73821&location=424&nothanks=yes&sub=marine&utm_tryagain=yes"
 goto startline
 
 exit \b
