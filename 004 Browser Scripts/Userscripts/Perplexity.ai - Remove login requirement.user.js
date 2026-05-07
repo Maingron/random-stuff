106de077dc975d05630a66ba71af03422bff8b1a
Maingron <r@maingron.com>
2026-05-07 14:28:51 +0200

    [004] Perplexity.ai script: Add missing .user.js file extension

--- 004 Browser Scripts/Userscripts/Perplexity.ai - Remove login requirement.user.js
+++ 004 Browser Scripts/Userscripts/Perplexity.ai - Remove login requirement.user.js
@@ -6,9 +6,9 @@
 // @namespace        https://maingron.com/
 // @homepage         https://maingron.com/
 // @supportURL       https://github.com/Maingron/random-stuff/issues
-// @source           https://github.com/Maingron/random-stuff/blob/main/004%20Browser%20Scripts/Userscripts/Perplexity.ai%20-%20Remove%20login%20requirement.js
-// @downloadURL      https://raw.githubusercontent.com/Maingron/random-stuff/refs/heads/main/004%20Browser%20Scripts/Userscripts/Perplexity.ai%20-%20Remove%20login%20requirement.js
-// @updateURL        https://raw.githubusercontent.com/Maingron/random-stuff/refs/heads/main/004%20Browser%20Scripts/Userscripts/Perplexity.ai%20-%20Remove%20login%20requirement.js
+// @source           https://github.com/Maingron/random-stuff/blob/main/004%20Browser%20Scripts/Userscripts/Perplexity.ai%20-%20Remove%20login%20requirement.user.js
+// @downloadURL      https://raw.githubusercontent.com/Maingron/random-stuff/refs/heads/main/004%20Browser%20Scripts/Userscripts/Perplexity.ai%20-%20Remove%20login%20requirement.user.js
+// @updateURL        https://raw.githubusercontent.com/Maingron/random-stuff/refs/heads/main/004%20Browser%20Scripts/Userscripts/Perplexity.ai%20-%20Remove%20login%20requirement.user.js
 // @version          2026.05.2
 // @description      Remove login requirement on Perplexity.ai. This includes all annoying overlays, cookie banner and Login features.
 // @description:de   Login Bedingung auf Perpexity.ai entfernen. Dies entfernt alle nervigen Overlays, Cookie Banner und Login Features.
